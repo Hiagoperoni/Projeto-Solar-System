b5b81d5adc9ff058f93169fa3b6adf29288c54c2
HiagoP <hiagoperonip@gmail.com>
2022-08-18 18:05:58 -0300

    Todos os Requisitos completos

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -5,7 +5,7 @@ class MissionCard extends React.Component {
   render() {
     const { name, year, country, destination } = this.props;
     return (
-      <div className="missoes">
+      <div data-testid="mission-card" className="missoes">
         <p data-testid="mission-name">{ name }</p>
         <p data-testid="mission-year">{ year }</p>
         <p data-testid="mission-country">{ country }</p>
