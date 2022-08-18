8487509908da443252fffcd4c262a8bb940be6d1
HiagoP <hiagoperonip@gmail.com>
2022-08-18 15:18:47 -0300

    Requisito 5

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import planets from '../data/planets';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <p>Solar System</p>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
